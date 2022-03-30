f8155e2d8e9d5d43e66fafe8e92c6ee8139bde87
AlexFerMar <afernandezmartinez@danielcastelao.org>
2022-03-30 15:07:24 +0200

    Añadido Boletin28

---
+++ TercerTrimestre/Boletin28/Boletin28_1/main.java
@@ -0,0 +1,15 @@
+package com.main;
+
+
+public class main {
+
+    
+    public static void main(String[] args) {
+        
+       Gui gui= new Gui();
+       
+       gui.setVisible(true);
+       gui.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ TercerTrimestre/Boletin28/Boletin28_2/main.java
@@ -0,0 +1,14 @@
+package com.main;
+
+public class main {
+
+    
+    public static void main(String[] args) {
+        
+        Gui gui= new Gui();
+       
+       gui.setVisible(true);
+       gui.setLocationRelativeTo(null);
+    }
+    
+}
