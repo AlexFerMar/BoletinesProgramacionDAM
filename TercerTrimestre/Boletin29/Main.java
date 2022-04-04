296327918e0672cc8e90ae176754ba83de7dd4ac
Alex Fernandez Martinez <afernandezmartinez@danielcastelao.org>
2022-04-04 10:45:46 +0200

    Añadido Boletin29

---
+++ TercerTrimestre/Boletin29/Main.java
@@ -0,0 +1,15 @@
+package com.Main;
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+       
+        Boletin29 gui=new Boletin29();
+        
+        gui.setVisible(true);
+        
+        
+    }
+    
+}
